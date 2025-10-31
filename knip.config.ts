68ea3d36121665c4289109c1f09d53150a378918
hobbescodes <87732294+hobbescodes@users.noreply.github.com>
2025-10-31 10:11:03 -0500

    refactor: add workspace biome configs, update knip config/usage

---
+++ knip.config.ts
@@ -0,0 +1,38 @@
+import type { KnipConfig } from "knip";
+
+export default {
+  workspaces: {
+    "packages/*": {
+      project: ["src/**/*.{ts,tsx}"],
+      ignore: [
+        "build/**",
+        "lib/**",
+        "dist/**",
+        "node_modules/**",
+        // TODO: remove when exported types from this files are used (rdk workspace). Add individual ignore statements as needed
+        "src/lib/types/xr/XRUtils.type.ts",
+      ],
+    },
+    "apps/*": {
+      entry: ["src/index.tsx"],
+      project: ["src/**/*.{ts,tsx}"],
+      ignore: ["build/**", "dist/**", "node_modules/**"],
+      ignoreDependencies: [
+        // TODO: remove when used within an app level workspace
+        "@react-three/drei",
+      ],
+    },
+  },
+  rules: {
+    binaries: "off",
+  },
+  ignore: ["**/*.test.{ts,tsx}", "**/*.spec.{ts,tsx}", "**/.turbo/**"],
+  ignoreDependencies: [
+    // NB: we use `turbo` at root level, thus biome presents as a false positive
+    "@biomejs/biome",
+  ],
+  ignoreExportsUsedInFile: {
+    interface: true,
+    type: true,
+  },
+} satisfies KnipConfig;
