a168b03aaac6f5f05a519fb0f40daffe82fb1747
Brian Cooper <brian@brian-cooper.com>
2025-11-02 17:14:12 -0800

    refactor(demo): update demos to use extracted XR API

--- apps/fiducial-demo/src/App.tsx
+++ apps/fiducial-demo/src/App.tsx
@@ -1,4 +1,5 @@
-import { XRCanvas, FiducialAnchor } from "@omnidotdev/rdk";
+import { XR, FiducialAnchor, FiducialSession } from "@omnidotdev/rdk";
+import { Canvas } from "@react-three/fiber";
 
 import { Supertorus } from "components";
 
@@ -6,7 +7,7 @@ import { Supertorus } from "components";
  * Fiducial marker-based AR demo application.
  */
 const App = () => (
-	<XRCanvas
+	<Canvas
 		gl={{
 			antialias: false,
 			powerPreference: "default",
@@ -16,17 +17,21 @@ const App = () => (
 		<hemisphereLight intensity={0.6} />
 		<directionalLight position={[5, 5, 5]} intensity={1} />
 
-		<FiducialAnchor params={{ smooth: true }} patternUrl="/data/rdk.patt">
-			<Supertorus
-				R={4}
-				n={10}
-				t={1.5}
-				position={[0, 0, 0]}
-				scale={0.25}
-				rotation={[Math.PI / 2, 0, 0]}
-			/>
-		</FiducialAnchor>
-	</XRCanvas>
+		<XR cameraSource="video">
+			<FiducialSession>
+				<FiducialAnchor params={{ smooth: true }} patternUrl="/data/rdk.patt">
+					<Supertorus
+						R={4}
+						n={10}
+						t={1.5}
+						position={[0, 0, 0]}
+						scale={0.25}
+						rotation={[Math.PI / 2, 0, 0]}
+					/>
+				</FiducialAnchor>
+			</FiducialSession>
+		</XR>
+	</Canvas>
 );
 
 export default App;
