20daaf5bfb04aa96ea8328de3835fe7d290f6851
Brian Cooper <brian@brian-cooper.com>
2025-12-30 00:55:44 -0800

    chore: remove dead code

--- packages/rdk/src/lib/util/immersive/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as fromWebXRSessionMode } from "./fromWebXRSessionMode";
-export { default as isImmersiveModeSupported } from "./isImmersiveModeSupported";
-export { default as toWebXRSessionMode } from "./toWebXRSessionMode";
